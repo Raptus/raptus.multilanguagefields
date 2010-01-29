ec42d3b0a9f8ace57cd61870c9318500c2e002de
Simon Käser <skaeser@raptus.com>
2010-01-29 14:28:58 +0000

    changed criterion indices registration to avoid warning messages on startup

--- raptus/multilanguagefields/criteria.py
+++ raptus/multilanguagefields/criteria.py
@@ -25,12 +25,23 @@ RELEVANT_INDICES.remove('MultilanguageDateRangeIndex')
 RELEVANT_INDICES.remove('DateRangeIndex')
 RELEVANT_INDICES = tuple(RELEVANT_INDICES)
 
-criteria.registerCriterion(criteria.ATSortCriterion, SORT_INDICES + criteria.SORT_INDICES)
-criteria.registerCriterion(criteria.ATSimpleStringCriterion, STRING_INDICES + criteria.STRING_INDICES)
-criteria.registerCriterion(criteria.ATSimpleIntCriterion, LIST_INDICES + criteria.LIST_INDICES)
-criteria.registerCriterion(criteria.ATSelectionCriterion, LIST_INDICES + criteria.LIST_INDICES)
-criteria.registerCriterion(criteria.ATListCriterion, LIST_INDICES + criteria.LIST_INDICES)
-criteria.registerCriterion(criteria.ATReferenceCriterion, REFERENCE_INDICES + criteria.REFERENCE_INDICES)
-criteria.registerCriterion(criteria.ATDateRangeCriterion, RELEVANT_INDICES)
-criteria.registerCriterion(criteria.ATDateCriteria, DATE_INDICES + criteria.DATE_INDICES)
-criteria.registerCriterion(criteria.ATBooleanCriterion, FIELD_INDICES + criteria.FIELD_INDICES)
+def setCriterionIndices(criterion, indices):
+    registry = criteria._criterionRegistry
+    crit_id = criterion.meta_type
+    registry[crit_id] = criterion
+    registry.portaltypes[criterion.portal_type] = criterion
+
+    registry.criterion2index[crit_id] = indices
+    for index in indices:
+        value = registry.index2criterion.get(index, ())
+        registry.index2criterion[index] = value + (crit_id,)
+
+setCriterionIndices(criteria.ATSortCriterion, SORT_INDICES + criteria.SORT_INDICES)
+setCriterionIndices(criteria.ATSimpleStringCriterion, STRING_INDICES + criteria.STRING_INDICES)
+setCriterionIndices(criteria.ATSimpleIntCriterion, LIST_INDICES + criteria.LIST_INDICES)
+setCriterionIndices(criteria.ATSelectionCriterion, LIST_INDICES + criteria.LIST_INDICES)
+setCriterionIndices(criteria.ATListCriterion, LIST_INDICES + criteria.LIST_INDICES)
+setCriterionIndices(criteria.ATReferenceCriterion, REFERENCE_INDICES + criteria.REFERENCE_INDICES)
+setCriterionIndices(criteria.ATDateRangeCriterion, RELEVANT_INDICES)
+setCriterionIndices(criteria.ATDateCriteria, DATE_INDICES + criteria.DATE_INDICES)
+setCriterionIndices(criteria.ATBooleanCriterion, FIELD_INDICES + criteria.FIELD_INDICES)
